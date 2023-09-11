3d9c3ffc167aa067eb1b1dececf284ecadc4d4aa
Swift <swiftx37@gmail.com>
2023-09-11 09:09:56 +0200

    Configuration (IdentityServer4, EntityFramework)

---
+++ Notes.Identity/Data/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Notes.Identity.Data.Context;
+
+namespace Notes.Identity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(AuthDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
--- Notes.Identity/Program.cs
+++ Notes.Identity/Program.cs
@@ -1,5 +1,63 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Data.Context;
+using Notes.Identity.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 8;
+    config.Password.RequireDigit = true;
+    config.Password.RequireLowercase = true;
+    config.Password.RequireUppercase = false;
+    config.Password.RequireNonAlphanumeric = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var context = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, "An error occurred while app initialization");
+    }
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.MapGet("/", () => "Hello World!");
 
